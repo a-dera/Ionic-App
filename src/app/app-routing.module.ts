f7ae661f4b9b8050b9122ca185cf5d7c5a224937
Amédée DERA <dera.amedee@gmail.com>
2021-01-27 20:39:40 +0000

    Ajout de la page d'accueil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'accueil',
+    loadChildren: () => import('./accueil/accueil.module').then( m => m.AccueilPageModule)
+  },
 ];
 
 @NgModule({
